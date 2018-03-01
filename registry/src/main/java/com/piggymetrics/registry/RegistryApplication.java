ec7dc2b0d78b3e2b1594eb2005668f3ba666f80b
jaredhu <jaredhu@vip.qq.com>
2018-03-01 17:11:16 +0800

    spring cloud 版本升级到 Edgware.SR2，config配置加密，界面mongodb 密码验证连接问题

--- registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
+++ registry/src/main/java/com/piggymetrics/registry/RegistryApplication.java
@@ -2,10 +2,12 @@ package com.piggymetrics.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigurationProperties
 public class RegistryApplication {
 
 	public static void main(String[] args) {
